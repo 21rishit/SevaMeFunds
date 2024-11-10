16826097e30f499c6ed76d44b465f5492631bec0
Rishit <prishitosh@gmail.com>
2024-11-11 00:13:43 +0530

    minor bug fixex

--- client/src/store.js
+++ client/src/store.js
@@ -10,19 +10,5 @@ const rootReducer = combineReducers({
 });
 
 export const store = configureStore({
-    reducer: rootReducer, // Pass the combined rootReducer
+    reducer: rootReducer,
 });
-
-// export store;
-
-
-// const store = createStore(rootReducer);
-
-// export default store;
-
-// export const store = configureStore({
-//     reducer: {
-//         user: userReducer, // 'user' will manage the state for userSlice
-//         ngo: ngoReducer,   // 'ngo' will manage the state for ngoSlice
-//     },
-// });
